f20b836f59ff75e9c226371411e9182010caf3b3
lhphuoc2311 <phuoc2311@gmail.com>
2022-03-29 14:15:25 +0700

    C Project Build+Compile Module Full

---
+++ src/StudentImpl.c
@@ -0,0 +1,5 @@
+#include "../include/StudentInt.h"
+
+int sub(int a, int b){
+    return a - b;
+}
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "../include/Interface.h"
+#include "../include/StudentInt.h"
+
+
+int main()
+
+{
+	printf("Hello World!\n");
+
+	printf("Hello World!\n");
+
+	printf("Hello World!\n");
+
+	int sum = add(5, 6);
+
+
+	int subtract = sub(6, 5);
+
+	printf("Sum = %d \n", sum);
+
+	printf("Subtract = %d \n", subtract);
+
+	getchar();
+	return (0);
+}
